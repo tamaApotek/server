93acda7bdbaa86676061d4697da250e786d68266
restuu <restu_u@outlook.com>
2019-11-17 19:17:22 +0700

    Feat: DoctorSchedule Model

--- src/model/doctorSchedule.ts
+++ src/model/doctorSchedule.ts
@@ -1,3 +1,15 @@
+export interface Schedule {
+  assistants: string[];
+  /** iso day week 1 ~ 7 */
+  dayOfWeek: number;
+  startHour: number;
+  startMinute: number;
+  endHour: number;
+  endMinute: number;
+}
+
 export interface DoctorSchedule {
   doctorID: string;
+  assistants: string[];
+  schedules: Schedule[];
 }
